f0617782b4999ab5c2c7f2d0d9788c595191f9c4
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-04-29 13:09:45 +0530

    Nav and image slider added

---
+++ pagesJs/slider.js
@@ -0,0 +1,32 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+
+
+    // Slide images according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    // Show or hide slide buttons based on scroll position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    }
+
+
+
+    // Call these two functions when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+}
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
